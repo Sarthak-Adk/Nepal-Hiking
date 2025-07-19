b7bb1dddaf7a573588c727ab1217c47e9b931f65
Sarthak-Adk <adhikarisarthak33@gmail.com>
2025-07-19 08:19:11 +0545

    Editing the theme and background

--- script.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('scroll', function() {
-    const navbar = document.getElementById('navbar');
-    if (window.scrollY > 200) {
-        navbar.classList.add('scrolled');
-    } else {
-        navbar.classList.remove('scrolled');
-    }
-});
